2f5ff232b426dece3d2bfbaff3a74873d7b2180a
So Jeong Lee <sj27419@gmail.com>
2022-01-13 01:06:53 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,23 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl + shift + L 단축키 전체 목록보기
+		// ctrl + +/- 확대 축소
+		// ctrl + d 한줄 삭제
+		// ctrl + alt + down 행단위 복사
+		// alt + shift + a 멀티컬럼 편집
+		// alt + up/down 행단위 이동
+		// ctrl + i 자동 들여쓰기
+        // ctrl + / 주석처리 (토글)	
+		// ctrl + space 자동완성
+		System.out.println();
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+	
+	}
+
+}
